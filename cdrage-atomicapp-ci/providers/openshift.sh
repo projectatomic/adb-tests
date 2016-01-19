79004c4dc50047e46e0a6e827bc3544a489d5c0b
Charlie Drage <charlie@charliedrage.com>
2016-01-19 10:34:33 -0500

    Modify way OpenShift provider is setup

--- cdrage-atomicapp-ci/providers/openshift.sh
+++ cdrage-atomicapp-ci/providers/openshift.sh
@@ -15,7 +15,11 @@ start_openshift() {
     -v /:/rootfs:ro -v /var/run:/var/run:rw -v /sys:/sys -v /var/lib/docker:/var/lib/docker:rw \
     -v /var/lib/origin/openshift.local.volumes:/var/lib/origin/openshift.local.volumes \
     openshift/origin start
-  sleep 15
+  until nc -z 127.0.0.1 8443;
+  do
+      echo ...
+      sleep 1
+  done
 }
 
 stop_openshift() {
@@ -37,15 +41,17 @@ answers_openshift() {
   GENERATING OPENSHIFT ANSWERS FILE
   ##########
   "
-  # Create a new project and give standard openshift user permission
-  docker exec -it origin oc new-project foo
-  sleep 2
-  docker exec -it origin oc policy add-role-to-user admin openshift -n foo
-  sleep 2
 
   # Retrieve the API key to use
   API_KEY=`curl -k -L -D - -u openshift:openshift -H 'X-CSRF-Token: 1' 'https://localhost:8443/oauth/authorize?response_type=token&client_id=openshift-challenging-client' 2>&1 | grep -oP "access_token=\K[^&]*"`
   echo $API_KEY
+
+  docker exec -it origin oc config set-credentials openshift --token=$API_KEY
+  docker exec -it origin oc config set-cluster openshift1 --server=https://localhost:8443 --insecure-skip-tls-verify=true
+  docker exec -it origin oc config set-context openshift --cluster=openshift1 --user=openshift
+  docker exec -it origin oc config use-context openshift
+  docker exec -it origin oc config set contexts.openshift.namespace foo
+  docker exec -it origin oc new-project foo
 
   echo "[general]
   provider = openshift

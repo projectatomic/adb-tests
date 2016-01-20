559eb1a67cb42968b92997a1ad575f8169275828
Charlie Drage <charlie@charliedrage.com>
2016-01-20 09:59:06 -0500

    Change k8s deploy to wait for port open

--- cdrage-atomicapp-ci/providers/kubernetes.sh
+++ cdrage-atomicapp-ci/providers/kubernetes.sh
@@ -11,7 +11,11 @@ start_k8s() {
   docker run --net=host -d gcr.io/google_containers/etcd:2.0.9 /usr/local/bin/etcd --addr=127.0.0.1:4001 --bind-addr=0.0.0.0:4001 --data-dir=/var/etcd/data
   docker run --net=host -d -v /var/run/docker.sock:/var/run/docker.sock gcr.io/google_containers/hyperkube:v0.21.2 /hyperkube kubelet --api_servers=http://localhost:8080 --v=2 --address=0.0.0.0 --enable_server --hostname_override=127.0.0.1 --config=/etc/kubernetes/manifests
   docker run -d --net=host --privileged gcr.io/google_containers/hyperkube:v0.21.2 /hyperkube proxy --master=http://127.0.0.1:8080 --v=2
-  sleep 15
+  until nc -z 127.0.0.1 8080;
+  do
+      echo ...
+      sleep 1
+  done
 }
 
 # Delete via image name google_containers

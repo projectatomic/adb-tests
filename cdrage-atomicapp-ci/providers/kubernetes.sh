d6747c3ee151ec3d2b2c302f4d2ba5482e4cff8b
Charlie Drage <charlie@charliedrage.com>
2016-02-23 10:54:11 -0500

    Switch to 1.2.7alpha of Kubernetes to reflect most recent changes

--- cdrage-atomicapp-ci/providers/kubernetes.sh
+++ cdrage-atomicapp-ci/providers/kubernetes.sh
@@ -8,9 +8,30 @@ start_k8s() {
   STARTING KUBERNETES
   ##########
   "
-  docker run --net=host -d gcr.io/google_containers/etcd:2.0.9 /usr/local/bin/etcd --addr=127.0.0.1:4001 --bind-addr=0.0.0.0:4001 --data-dir=/var/etcd/data
-  docker run --net=host -d -v /var/run/docker.sock:/var/run/docker.sock gcr.io/google_containers/hyperkube:v0.21.2 /hyperkube kubelet --api_servers=http://localhost:8080 --v=2 --address=0.0.0.0 --enable_server --hostname_override=127.0.0.1 --config=/etc/kubernetes/manifests
-  docker run -d --net=host --privileged gcr.io/google_containers/hyperkube:v0.21.2 /hyperkube proxy --master=http://127.0.0.1:8080 --v=2
+  # Use alpha for now since this contains the new hyperkube format going forward
+  K8S_VERSION=1.2.0-alpha.7
+  docker run \
+  --volume=/:/rootfs:ro \
+  --volume=/sys:/sys:ro \
+  --volume=/var/lib/docker/:/var/lib/docker:rw \
+  --volume=/var/lib/kubelet/:/var/lib/kubelet:rw \
+  --volume=/var/run:/var/run:rw \
+  --net=host \
+  --pid=host \
+  --privileged=true \
+  -d \
+  gcr.io/google_containers/hyperkube-amd64:v${K8S_VERSION} \
+  /hyperkube kubelet \
+      --containerized \
+      --hostname-override="127.0.0.1" \
+      --address="0.0.0.0" \
+      --api-servers=http://localhost:8080 \
+      --config=/etc/kubernetes/manifests \
+      --cluster-dns=10.0.0.10 \
+      --cluster-domain=cluster.local \
+      --allow-privileged=true --v=2
+
+
   until nc -z 127.0.0.1 8080;
   do
       echo ...
@@ -18,9 +39,6 @@ start_k8s() {
   done
 }
 
-# Delete via image name google_containers
-# Delete all containers started (names start with k8s_)
-# Run twice in-case a container is replicated during that time
 
 stop_k8s() {
   echo "
@@ -28,8 +46,15 @@ stop_k8s() {
   STOPPING KUBERNETES
   ##########
   "
-   docker ps -a | grep 'gcr.io/google_containers' | awk '{print $1}' | xargs --no-run-if-empty docker rm -f || true
-   docker ps -a | grep 'k8s_' | awk '{print $1}' | xargs --no-run-if-empty docker rm -f || true
+  # Delete via image name google_containers
+  # Delete all containers started (names start with k8s_)
+  # Run twice in-case a container is replicated during that time
+  echo "\n-----Removing all k8s containers-----\n"
+  for run in {0..2}
+  do
+    docker ps -a | grep 'k8s_' | awk '{print $1}' | xargs --no-run-if-empty docker rm -f
+    docker ps -a | grep 'gcr.io/google_containers/hyperkube-amd64' | awk '{print $1}' | xargs --no-run-if-empty docker rm -f
+  done
 }
 
 answers_k8s() {
@@ -40,20 +65,15 @@ answers_k8s() {
   "
   echo "[general]
   provider = kubernetes
-  namespace = foo" > answers.conf
+  namespace = default" > answers.conf
 }
 
-case "$1" in
-        start)
-            start_k8s 
-            ;;
-        stop)
-            stop_k8s 
-            ;;
-        answers)
-            answers_k8s
-            ;;
-        *)
-            echo $"Usage: kubernetes.sh {start|stop|answers}"
-            exit 1
-esac
+if [[ $1 == "answers" ]]; then
+  answers_k8s
+elif [[ $1 == "start" ]]; then
+  start_k8s
+elif [[ $1 == "stop" ]]; then
+  stop_k8s
+else
+  echo $"Usage: kubernetes.sh {answers|start|stop}"
+fi

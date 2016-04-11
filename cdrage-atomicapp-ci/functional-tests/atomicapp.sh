0f94169cc2bfecba3e30de741e46b8471ecd445d
Charlie Drage <charlie@charliedrage.com>
2016-04-11 15:48:16 -0400

    Remove make test from atomicapp.sh

--- cdrage-atomicapp-ci/functional-tests/atomicapp.sh
+++ cdrage-atomicapp-ci/functional-tests/atomicapp.sh
@@ -28,9 +28,6 @@ install_atomicapp() {
       curl -s "https://patch-diff.githubusercontent.com/raw/${UPSTREAM}/pull/${1}.patch" | git am
   fi
 
-  # Test first :)
-  make test
-
   # Install
   make install
 

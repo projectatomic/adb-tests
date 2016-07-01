f0c2fff61d22be692c0d0aff9f47145f989294ba
naina-verma <naverma@redhat.com>
2016-07-01 19:10:11 +0530

    updated

--- cdk-testsuite/tests/cmd.py
+++ cdk-testsuite/tests/cmd.py
@@ -4,60 +4,101 @@ from avocado import Test
 import subprocess
 import os
 import pexpect
-#from Logic import Utils
+#from Logic import vagrant_lib
 
-Utils= imp.load_source('Utils', '../libraries/vagrant/vagrant.py')
+vagrant_lib= imp.load_source('vagrant_lib', '/cygdrive/c/Users/naina/Downloads/ACDK/ACDK/Logic/Utils.py')
 
 class cmd(Test):
 
 
-    def test_Rhel_hyper_k8_up(self):
+    
+                
+    
+    def test_vagrant_up(self):
         self.log.info("Getting Path to CDK directory")
-
-        path_var = Utils.settingPath(self)
-        try:
-            globalstatus = Utils.global_status(self, "misc/hyperv/rhel-k8s-singlenode-hyperv")
-            self.log.info(
-                "Status of " + path_var + 'misc/hyperv/rhel-k8s-singlenode-hyperv' + "  " + globalstatus.strip())
-        except Exception:
-            self.log.info("Exception!!")
-            globalstatus = 'none'
-        finally:
-
-            if (globalstatus.strip() != 'running'):
-                self.log.info("Status of " + path_var + 'misc/hyperv/rhel-k8s-singlenode-hyperv' + "  " + globalstatus)
-
-                (output, err) = Utils.vagrantUp(self, path_var + 'misc/hyperv/rhel-k8s-singlenode-hyperv')
+        vagrnat_up_dirs=self.params.get('vagrant_UP_FILES')
+        self.log.info('These are the locations of Vagrant files ::')
+        
+        for x in vagrnat_up_dirs.split(',') :
+            self.log.info(x)
+            path_var = self.params.get('vagrant_VARGRANTFILE_DIRS')
+            try:
+                (output, err) = vagrant_lib.vagrantUp(self, path_var + x)
                 self.log.debug("Sleep for 40 seconds : Before checking global status")
-                globalstatus = Utils.global_status(self, "misc/hyperv/rhel-k8s-singlenode-hyperv")
-                self.log.info("Status of " + path_var + 'misc/hyperv/rhel-k8s-singlenode-hyperv' + "  " + globalstatus)
-                assert globalstatus.strip(), 'running'
+                globalstatus = vagrant_lib.global_status(self, path_var+x)
+                self.log.info("Status of 1" + path_var + x + "  " + globalstatus+"***")
+                assert globalstatus, 'running'
+               
+            except Exception:
+                self.log.info("Exception!!")
+                
+            finally:
+                globalstatus = vagrant_lib.global_status(self,path_var+ x)
+                self.log.info("Status of 2" + path_var + x + "  " + str( globalstatus )+"***")
+                if  'running' in globalstatus :
+                    self.log.info("Status of " + path_var + x + "  " +str( globalstatus)+"***")
+                    (output, err) = vagrant_lib.vagrantUp(self, path_var + x)
+                    self.log.debug("Sleep for 40 seconds : Before checking global status")
+                    time.sleep(40)
+                    globalstatus = vagrant_lib.global_status(self, path_var+x)
+                    self.log.info("Status of 3" + path_var + x + "  " + str(globalstatus))
+                    assert globalstatus, 'running'
 
-            elif (globalstatus.strip() == 'running'):
+                    
 
-                globalstatus = Utils.global_status(self, "misc/hyperv/rhel-k8s-singlenode-hyperv")
-                self.log.info("Status of " + path_var + 'misc/hyperv/rhel-k8s-singlenode-hyperv' + "  " + globalstatus)
+                elif 'running' not in globalstatus :
 
-    def test_k8_powershell_suspend(self):
-        Utils.shell_commands(self,'vagrant status')
+                    globalstatus = vagrant_lib.global_status(self,path_var+ x)
+                    self.log.info("Status of " + path_var + x + "  " + str(globalstatus))
+
+
+
+    def test_k8_powershell_status(self):
+        if self.params.get('Windows_Shell') == 'powershell':
+            operator = '|'
+        elif self.params.get('Windows_Shell') == 'cmd':
+            operator = '&'
+        else:
+            operator='&&'
+        vagrnat_up_dirs=self.params.get('vagrant_UP_FILES')
+        self.log.info('These are the locations of Vagrant files ::')
+        
+        for x in vagrnat_up_dirs.split(',') :
+
+            output=vagrant_lib.shell_commands(self,'cd ' + self.params.get('vagrant_VARGRANTFILE_DIRS') + x+' ' + operator + ' vagrant status' )
+            self.assertTrue('running' in output)
+        
 
 
 
     def test_k8_powershell_resume(self):
-        Utils.shell_commands(self, 'vagrant resume')
-
-    def test_k8_powershell_halt(self):
-        Utils.shell_commands(self, 'vagrant halt')
-        psxmlgen = subprocess.Popen([r'C:/WINDOWS/system32/WindowsPowerShell/v1.0/powershell.exe',
-                                     'cd ' + self.params.get(
-                                         'vagrant_VARGRANTFILE_DIRS') + 'misc/hyperv/rhel-k8s-singlenode-hyperv | vagrant halt'],
-                                    cwd=os.getcwd())
-        result = psxmlgen.wait
-        time.sleep(20)
-        self.log.debug('Resume result :' + str(result))
+        if self.params.get('Windows_Shell') == 'powershell':
+            operator = '|'
+        elif self.params.get('Windows_Shell') == 'cmd':
+            operator = '&'
+        else:
+            operator='&&'
+        vagrnat_up_dirs=self.params.get('vagrant_UP_FILES')
+        self.log.info('These are the locations of Vagrant files ::')
+        
+        for x in vagrnat_up_dirs.split(',') :
+            output=vagrant_lib.shell_commands(self, 'cd ' + self.params.get('vagrant_VARGRANTFILE_DIRS') + x+' ' + operator + 'vagrant resume')
+            self.assertTrue('Resuming' in output)
+    
 
     def test_k8_powershell_destroy(self):
-       Utils.shell_commands(self, 'vagrant destroy --force')
+        if self.params.get('Windows_Shell') == 'powershell':
+            operator = '|'
+        elif self.params.get('Windows_Shell') == 'cmd':
+            operator = '&'
+        else:
+            operator='&&'
+        vagrnat_up_dirs=self.params.get('vagrant_UP_FILES')
+        self.log.info('These are the locations of Vagrant files ::')
+        
+        for x in vagrnat_up_dirs.split(',') :
+            output=vagrant_lib.shell_commands(self,  'cd ' + self.params.get('vagrant_VARGRANTFILE_DIRS') + x+' ' + operator + 'vagrant destroy --force')
+            self.assertTrue('Deleting' in output)  
 
 
 

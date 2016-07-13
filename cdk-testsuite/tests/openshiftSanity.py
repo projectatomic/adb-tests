e8e521bb406a23d20e00dae982ff1ebf19e0b2e6
amitkrout <amit.silicon2008@hotmail.com>
2016-07-13 12:28:43 +0530

    Updated openshift testcase file for openshift sanity revision 2
    Generic test cases are created

--- cdk-testsuite/tests/openshiftSanity.py
+++ cdk-testsuite/tests/openshiftSanity.py
@@ -35,165 +35,43 @@ class OpenshiftTests(Test):
         self.log.info("Avocado version : %s" % VERSION)
         self.log.info("###########################################################################################")
         
-    def test_oc_new_python_project(self):
+    def test_python_project(self):
         '''
         TBD
         '''
-        output = openshift.oc_usr_login(self, self.params.get('openshift_console_URL'), self.params.get('openshift_login_USERNAME'), self.params.get('openshift_login_PASSWORD'))
-        self.log.info(output)
-        self.assertIn("Login successful", output, "Login failed")
-        
-        output = openshift.add_new_project(self, self.params.get('openshift_new_python_PROJECT'))
-        self.assertIn(self.params.get('openshift_new_python_PROJECT'), output, "Failed to create " +self.params.get('openshift_new_python_PROJECT'))
-        
-        output = openshift.add_new_app(self, self.params.get('openshift_python_REGISTRY'))
-        partenLst = []
-        lst = self.params.get('openshift_python_REGISTRY').split("/")
-        repo = lst[len(lst) - 1]
-        for lines in output.splitlines():
-            parten = re.search(r"^(?=.*?\b\\*\b)(?=.*?\bfailed\b)(?=.*?\b%s\b).*$" %repo, lines)
-            partenLst.append(parten)
-        match = "NotFound"
-        for i in partenLst:
-            if i != None:
-                match = "Found"
-                break
-        self.assertIn("NotFound", match, self.params.get('openshift_python_REGISTRY') +" deployment failed")
-        
-        output = openshift.oc_port_expose(self, self.params.get('service_python_NAME'))
-        self.assertIn("exposed", output, "Service failed to expose " +self.params.get('openshift_new_python_PROJECT'))
-                                
-        time.sleep(5)
-        output = openshift.xip_io(self, self.params.get('service_python_NAME'), self.params.get('openshift_new_python_PROJECT'))
-        self.assertIn("HTTP/1.1 200 OK", output, "curl -I http://" +self.params.get('service_python_NAME') +"-" +self.params.get('openshift_new_python_PROJECT') +".rhel-cdk.10.1.2.2.xip.io/ fail to expose to outside")
-                                    
-        output = openshift.oc_get_pod(self)
-        self.assertIn("Running", output, "Failed to run pod")
-                                        
-        output = openshift.oc_delete(self, self.params.get('openshift_new_python_PROJECT'))
-        self.assertIn("deleted", output, "Failed to delete " +self.params.get('openshift_new_python_PROJECT'))
-                                        
-    def test_oc_new_ruby_project(self):
-        '''
-        TBD
-        '''
-        output = openshift.oc_usr_login(self, self.params.get('openshift_console_URL'), self.params.get('openshift_login_USERNAME'), self.params.get('openshift_login_PASSWORD'))
-        self.log.info(output)
-        self.assertIn("Login successful", output, "Login failed")
-        
-        output = openshift.add_new_project(self, self.params.get('openshift_new_ruby_PROJECT'))
-        self.assertIn(self.params.get('openshift_new_ruby_PROJECT'), output, "Failed to create " +self.params.get('openshift_new_ruby_PROJECT'))
-        
-        output = openshift.add_new_app(self, self.params.get('openshift_ruby_REGISTRY'))
-        partenLst = []
-        lst = self.params.get('openshift_ruby_REGISTRY').split("/")
-        repo = lst[len(lst) - 1]
-        for lines in output.splitlines():
-            parten = re.search(r"^(?=.*?\b\\*\b)(?=.*?\bfailed\b)(?=.*?\b%s\b).*$" %repo, lines)
-            partenLst.append(parten)
-        match = "NotFound"
-        for i in partenLst:
-            if i != None:
-                match = "Found"
-                break
-        self.assertIn("NotFound", match, self.params.get('openshift_ruby_REGISTRY') +" deployment failed")
-        
-        output = openshift.oc_port_expose(self, self.params.get('service_ruby_NAME'))
-        self.assertIn("exposed", output, "Service failed to expose " +self.params.get('openshift_new_python_PROJECT'))
-                                
-        time.sleep(5)
-        output = openshift.xip_io(self, self.params.get('service_ruby_NAME'), self.params.get('openshift_new_ruby_PROJECT'))
-        self.assertIn("HTTP/1.1 200 OK", output, "curl -I http://" +self.params.get('service_ruby_NAME') +"-" +self.params.get('openshift_new_ruby_PROJECT') +".rhel-cdk.10.1.2.2.xip.io/ fail to expose to outside")
-                                    
-        output = openshift.oc_get_pod(self)
-        self.assertIn("Running", output, "Failed to run pod")
-                                        
-        output = openshift.oc_delete(self, self.params.get('openshift_new_ruby_PROJECT'))
-        self.assertIn("deleted", output, "Failed to delete " +self.params.get('openshift_new_ruby_PROJECT'))
+        #openshift.new_project(self, self.params.get('openshift_console_URL'), self.params.get('openshift_login_USERNAME'), self.params.get('openshift_login_PASSWORD'), self.params.get('openshift_new_python_PROJECT'), self.params.get('openshift_python_REGISTRY'), self.params.get('service_python_NAME'))
+        openshift.new_project(self, self.params.get('openshift_URL'), self.params.get('openshift_USERNAME'), 
+                              self.params.get('openshift_PASSWORD'), self.params.get('openshift_python_PROJECT'), 
+                              self.params.get('openshift_python_REGISTRY'), self.params.get('service_python_NAME'))
     
-    def test_oc_new_perl_project(self):
+    def test_ruby_project(self):
         '''
         TBD
         '''
-        output = openshift.oc_usr_login(self, self.params.get('openshift_console_URL'), self.params.get('openshift_login_USERNAME'), self.params.get('openshift_login_PASSWORD'))
-        self.log.info(output)
-        self.assertIn("Login successful", output, "Login failed")
-        
-        output = openshift.add_new_project(self, self.params.get('openshift_new_perl_PROJECT'))
-        self.assertIn(self.params.get('openshift_new_perl_PROJECT'), output, "Failed to create " +self.params.get('openshift_new_perl_PROJECT'))
-        
-        output = openshift.add_new_app(self, self.params.get('openshift_perl_REGISTRY'))
-        partenLst = []
-        lst = self.params.get('openshift_perl_REGISTRY').split("/")
-        repo = lst[len(lst) - 1]
-        for lines in output.splitlines():
-            parten = re.search(r"^(?=.*?\b\\*\b)(?=.*?\bfailed\b)(?=.*?\b%s\b).*$" %repo, lines)
-            partenLst.append(parten)
-        match = "NotFound"
-        for i in partenLst:
-            if i != None:
-                match = "Found"
-                break
-        self.assertIn("NotFound", match, self.params.get('openshift_perl_REGISTRY') +" deployment failed")
-        
-        output = openshift.oc_port_expose(self, self.params.get('service_perl_NAME'))
-        self.assertIn("exposed", output, "Service failed to expose " +self.params.get('openshift_new_perl_PROJECT'))
-                                
-        time.sleep(5)
-        output = openshift.xip_io(self, self.params.get('service_perl_NAME'), self.params.get('openshift_new_perl_PROJECT'))
-        self.assertIn("HTTP/1.1 200 OK", output, "curl -I http://" +self.params.get('service_perl_NAME') +"-" +self.params.get('openshift_new_perl_PROJECT') +".rhel-cdk.10.1.2.2.xip.io/ fail to expose to outside")
-                                    
-        output = openshift.oc_get_pod(self)
-        self.assertIn("Running", output, "Failed to run pod")
-                                        
-        output = openshift.oc_delete(self, self.params.get('openshift_new_perl_PROJECT'))
-        self.assertIn("deleted", output, "Failed to delete " +self.params.get('openshift_new_perl_PROJECT'))
+        openshift.new_project(self, self.params.get('openshift_URL'), self.params.get('openshift_USERNAME'), 
+                              self.params.get('openshift_PASSWORD'), self.params.get('openshift_ruby_PROJECT'), 
+                              self.params.get('openshift_ruby_REGISTRY'), self.params.get('service_ruby_NAME'))
     
-    def test_oc_new_nodejs_project(self):
+    def test_perl_project(self):
         '''
         TBD
         '''
-        output = openshift.oc_usr_login(self, self.params.get('openshift_console_URL'), self.params.get('openshift_login_USERNAME'), self.params.get('openshift_login_PASSWORD'))
-        self.log.info(output)
-        self.assertIn("Login successful", output, "Login failed")
-        
-        output = openshift.add_new_project(self, self.params.get('openshift_new_nodejs_PROJECT'))
-        self.assertIn(self.params.get('openshift_new_nodejs_PROJECT'), output, "Failed to create " +self.params.get('openshift_new_nodejs_PROJECT'))
-        
-        output = openshift.add_new_app(self, self.params.get('openshift_nodejs_REGISTRY'))
-        partenLst = []
-        lst = self.params.get('openshift_nodejs_REGISTRY').split("/")
-        repo = lst[len(lst) - 1]
-        for lines in output.splitlines():
-            parten = re.search(r"^(?=.*?\b\\*\b)(?=.*?\bfailed\b)(?=.*?\b%s\b).*$" %repo, lines)
-            partenLst.append(parten)
-        match = "NotFound"
-        for i in partenLst:
-            if i != None:
-                match = "Found"
-                break
-        self.assertIn("NotFound", match, self.params.get('openshift_nodejs_REGISTRY') +" deployment failed")
-        
-        output = openshift.oc_port_expose(self, self.params.get('service_nodejs_NAME'))
-        self.assertIn("exposed", output, "Service failed to expose " +self.params.get('openshift_new_nodejs_PROJECT'))
-                                
-        time.sleep(5)
-        output = openshift.xip_io(self, self.params.get('service_nodejs_NAME'), self.params.get('openshift_new_nodejs_PROJECT'))
-        self.assertIn("HTTP/1.1 200 OK", output, "curl -I http://" +self.params.get('service_nodejs_NAME') +"-" +self.params.get('openshift_new_nodejs_PROJECT') +".rhel-cdk.10.1.2.2.xip.io/ fail to expose to outside")
-                                    
-        output = openshift.oc_get_pod(self)
-        self.assertIn("Running", output, "Failed to run pod")
-                                        
-        output = openshift.oc_delete(self, self.params.get('openshift_new_nodejs_PROJECT'))
-        self.assertIn("deleted", output, "Failed to delete " +self.params.get('openshift_new_nodejs_PROJECT'))
+        openshift.new_project(self, self.params.get('openshift_URL'), self.params.get('openshift_USERNAME'), 
+                              self.params.get('openshift_PASSWORD'), self.params.get('openshift_perl_PROJECT'), 
+                              self.params.get('openshift_perl_REGISTRY'), self.params.get('service_perl_NAME'))
+    
+    def test_nodejs_project(self):
+        '''
+        TBD
+        '''
+        openshift.new_project(self, self.params.get('openshift_URL'), self.params.get('openshift_USERNAME'), 
+                              self.params.get('openshift_PASSWORD'), self.params.get('openshift_nodejs_PROJECT'), 
+                              self.params.get('openshift_nodejs_REGISTRY'), self.params.get('service_nodejs_NAME'))
     
     def test_oc_logout(self):
         '''
         TBD
         '''
         output = openshift.oc_logout(self)
-        logout_str = "Logged " +"\"" +self.params.get('openshift_login_USERNAME') +"\"" +" out on " +"\"https://"+self.params.get('openshift_console_URL') +"\""
+        logout_str = "Logged " +"\"" +self.params.get('openshift_USERNAME') +"\"" +" out on " +"\"https://"+self.params.get('openshift_URL') +"\""
         self.assertIn(logout_str, output, "Failed to log out")
-
-
-            

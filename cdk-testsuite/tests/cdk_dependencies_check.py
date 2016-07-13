99b6fe94abb1a0df54411e7c14a8b6a392fad372
naina-verma <naverma@redhat.com>
2016-07-13 17:25:51 +0530

    Update cdk_dependencies_check.py

--- cdk-testsuite/tests/cdk_dependencies_check.py
+++ cdk-testsuite/tests/cdk_dependencies_check.py
@@ -3,7 +3,7 @@ from avocado import Test
 import subprocess
 import imp
 import vagrant
-cdk_utils = imp.load_source('cdk_utils', '/home/naina/PycharmProjects/abcdk/utils/cdk_dependency_util.py')
+cdk_utils = imp.load_source('cdk_utils', '../utils/cdk_dependency_util.py')
 class cdk_dependencies_check(Test):
 
 
@@ -19,10 +19,22 @@ class cdk_dependencies_check(Test):
         except:
             self.log.info("Vagrant is  not present on Machine ")
             cdk_utils.vagrant_installation(self,machine=machine,sudopassword=self.params.get('SudoPassword'))
+            time.sleep(30)
             vagrant_VERSION = vagrant_verification.version()
             self.log.info("Vagrant version is  :: " + vagrant_VERSION)
             self.assertTrue(vagrant_VERSION)
 
+    def test_enable_virtualization_verification(self):
+        self.log.info("Testing Enable Virtualization")
+        self.log.info("Checking which operating system are you using::")
+        machine = cdk_utils.platform_verification(self)
+        self.log.info("Verifying Vagrant for :: " +machine)
+        vagrant_verification = vagrant.Vagrant()
+
+        try:
+            vagrant_installation.enable_virtualization(self,machine=machine,sudopassword=self.params.get('SudoPassword'),sub_username=self.params.get('vagrant_RHN_USERNAME'),sub_password=self.params.get('vagrant_RHN_PASSWORD'))
+        except:
+            print "anything"
 
 
 

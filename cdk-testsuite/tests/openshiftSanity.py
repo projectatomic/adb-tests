b542aa45dc821c0a7b5d7978a83fb4a4e399cb31
amitkrout <amit.silicon2008@hotmail.com>
2016-07-19 14:11:47 +0530

    doc string added to openshift sanity
    doc string added to openshift sanity

--- cdk-testsuite/tests/openshiftSanity.py
+++ cdk-testsuite/tests/openshiftSanity.py
@@ -15,7 +15,9 @@ class OpenshiftTests(Test):
 
     def setUp(self):
         '''
-        TBD
+        preconfiguring the test setup before running each test case
+        Arg:
+            self (object): Object of the current method
         '''
         if os.name == "posix":
             os.chdir(self.params.get('path_linux'))
@@ -35,7 +37,23 @@ class OpenshiftTests(Test):
            
     def test_python_project(self):
         '''
-        TBD
+        Runs sanity on openshift s2i python source
+            1. login
+            2. new project create
+            3. App deploy using the registry from config file
+            4. Validation for successful deployment
+            5. Routing
+            6. Pod status
+            7. project delete
+            8. logout
+        Args:
+            self (object): Object of the current method
+            openshift_URL (string) : ip and port of openshift server to be used
+            openshift_USERNAME (string): username of openshift web console to be used
+            openshift_PASSWORD (string): password of openshift web console to be used
+            openshift_python_PROJECT (string): name of the python project to be added to the openshift server
+            openshift_python_REGISTRY (string): python registry path/location
+            service_python_NAME (string): name of the python service to be exposed outside
         '''
         openshift.new_project(self, self.params.get('openshift_URL'), self.params.get('openshift_USERNAME'), 
                               self.params.get('openshift_PASSWORD'), self.params.get('openshift_python_PROJECT'), 
@@ -43,7 +61,23 @@ class OpenshiftTests(Test):
     
     def test_ruby_project(self):
         '''
-        TBD
+        Runs sanity on openshift s2i ruby source
+            1. login
+            2. new project create
+            3. App deploy using the registry from config file
+            4. Validation for successful deployment
+            5. Routing
+            6. Pod status
+            7. project delete
+            8. logout
+        Args:
+            self (object): Object of the current method
+            openshift_URL (string) : ip and port of openshift server to be used
+            openshift_USERNAME (string): username of openshift web console to be used
+            openshift_PASSWORD (string): password of openshift web console to be used
+            openshift_ruby_PROJECT (string): name of the ruby project to be added to the openshift server
+            openshift_ruby_REGISTRY (string): ruby registry path/location
+            service_ruby_NAME (string): name of the ruby service to be exposed outside
         '''
         openshift.new_project(self, self.params.get('openshift_URL'), self.params.get('openshift_USERNAME'), 
                               self.params.get('openshift_PASSWORD'), self.params.get('openshift_ruby_PROJECT'), 
@@ -51,7 +85,23 @@ class OpenshiftTests(Test):
     
     def test_perl_project(self):
         '''
-        TBD
+        Runs sanity on openshift s2i perl source
+            1. login
+            2. new project create
+            3. App deploy using the registry from config file
+            4. Validation for successful deployment
+            5. Routing
+            6. Pod status
+            7. project delete
+            8. logout
+        Args:
+            self (object): Object of the current method
+            openshift_URL (string) : ip and port of openshift server to be used
+            openshift_USERNAME (string): username of openshift web console to be used
+            openshift_PASSWORD (string): password of openshift web console to be used
+            openshift_perl_PROJECT (string): name of the perl project to be added to the openshift server
+            openshift_perl_REGISTRY (string): perl registry path/location
+            service_perl_NAME (string): name of the perl service to be exposed outside
         '''
         openshift.new_project(self, self.params.get('openshift_URL'), self.params.get('openshift_USERNAME'), 
                               self.params.get('openshift_PASSWORD'), self.params.get('openshift_perl_PROJECT'), 
@@ -59,7 +109,23 @@ class OpenshiftTests(Test):
     
     def test_nodejs_project(self):
         '''
-        TBD
+        Runs sanity on openshift s2i nodejs source
+            1. login
+            2. new project create
+            3. App deploy using the registry from config file
+            4. Validation for successful deployment
+            5. Routing
+            6. Pod status
+            7. project delete
+            8. logout
+        Args:
+            self (object): Object of the current method
+            openshift_URL (string) : ip and port of openshift server to be used
+            openshift_USERNAME (string): username of openshift web console to be used
+            openshift_PASSWORD (string): password of openshift web console to be used
+            openshift_nodejs_PROJECT (string): name of the nodejs project to be added to the openshift server
+            openshift_nodejs_REGISTRY (string): nodejs registry path/location
+            service_nodejs_NAME (string): name of the nodejs service to be exposed outside
         '''
         openshift.new_project(self, self.params.get('openshift_URL'), self.params.get('openshift_USERNAME'), 
                               self.params.get('openshift_PASSWORD'), self.params.get('openshift_nodejs_PROJECT'), 
@@ -67,7 +133,24 @@ class OpenshiftTests(Test):
     
     def test_php_project(self):
         '''
-        TBD
+        Runs sanity on openshift s2i php source
+            1. login
+            2. new project create
+            3. App deploy using the registry from config file
+            4. Validation for successful deployment
+            5. Routing
+            6. Pod status
+            7. project delete
+            8. logout
+        Args:
+            self (object): Object of the current method
+            openshift_URL (string) : ip and port of openshift server to be used
+            openshift_USERNAME (string): username of openshift web console to be used
+            openshift_PASSWORD (string): password of openshift web console to be used
+            openshift_php_PROJECT (string): name of the php project to be added to the openshift server
+            openshift_php_template (string): name of php template
+            service_php_NAME (string): name of the php service to be exposed outside
+            tepmlate (boolean): True if using template
         '''
         openshift.new_project(self, self.params.get('openshift_URL'), self.params.get('openshift_USERNAME'), 
                               self.params.get('openshift_PASSWORD'), self.params.get('openshift_php_PROJECT'), 
@@ -76,7 +159,25 @@ class OpenshiftTests(Test):
     
     def test_nodejs_mongodb_template(self):
         '''
-        TBD
+        Runs sanity on openshift s2i nodejs with mongodb source
+            1. login
+            2. new project create
+            3. App deploy using the registry from config file
+            4. Validation for successful deployment
+            5. Routing
+            6. Pod status
+            7. project delete
+            8. logout
+        Args:
+            self (object): Object of the current method
+            openshift_URL (string) : ip and port of openshift server to be used
+            openshift_USERNAME (string): username of openshift web console to be used
+            openshift_PASSWORD (string): password of openshift web console to be used
+            openshift_nodejsmongodb_PROJECT (string): name of the nodejs with mongodb project to be added to the openshift server
+            openshift_nodejsmongodb_TEMPLATE (string): name of nodejs with mongodb template
+            service_nodejsmongodb_NAME (string): name of the nodejs service to be exposed outside
+            tepmlate (boolean): True if using template
+            dbservicename (string): Takes name of the database service name 
         '''
         openshift.new_project(self, self.params.get('openshift_URL'), self.params.get('openshift_USERNAME'), 
                               self.params.get('openshift_PASSWORD'), self.params.get('openshift_nodejsmongodb_PROJECT'), 
@@ -85,7 +186,9 @@ class OpenshiftTests(Test):
     
     def test_logout(self):
         '''
-        TBD
+        Loging out the test from openshift server
+        Args:
+            self (object): Object of the current method
         '''
         output = openshift.oc_logout(self)
         logout_str = "Logged " +"\"" +self.params.get('openshift_USERNAME') +"\"" +" out on " +"\"https://"+self.params.get('openshift_URL') +"\""

b2aec9ffe7d08838b00fe5b1e51af453914fae40
pavanbhat21 <pbhat@redhat.com>
2016-08-26 11:20:31 +0530

    Updated the script
    The script has been updated to handle:
    -> Vagrant file exceptions
    -> Sudo pwd prompted for Linux while vagrant up
    -> User added as passwordless sudoer
    -> Skip registration if registration plugin is not in required list in vagrant file
    -> Skip promt for registration if env variables are set for username/pwd

--- cdk-testsuite/tests/vagrant_sanity.py
+++ cdk-testsuite/tests/vagrant_sanity.py
@@ -2,17 +2,32 @@
 
 from avocado import Test
 from avocado.utils import process
-import os, re, time, pexpect, vagrant
+import os, re, pexpect, vagrant, platform, sys
 
 class VagrantSanity(Test):
     def setUp(self):
 	self.vagrant_VAGRANTFILE_DIR = self.params.get('vagrant_VAGRANTFILE_DIR')
-        self.vagrant_PROVIDER = self.params.get('vagrant_PROVIDER', default='')
+        self.vagrant_PROVIDER = self.params.get('vagrant_PROVIDER')
 	self.vagrant_RHN_USERNAME = self.params.get('vagrant_RHN_USERNAME')
 	self.vagrant_RHN_PASSWORD = self.params.get('vagrant_RHN_PASSWORD')
-	os.chdir(self.vagrant_VAGRANTFILE_DIR)
-	self.v = vagrant.Vagrant(self.vagrant_VAGRANTFILE_DIR)
-
+	self.platform = platform.system()
+	self.sudo_PASSWORD = self.params.get('sudo_PASSWORD')
+	self.suspended_state = ["paused", "saved"]
+	self.halt_state = ["off", "shutoff", "poweroff"]
+	self.registration_required = self.credentials_exported = False
+	self.vagrant_file = os.path.join(self.vagrant_VAGRANTFILE_DIR, "Vagrantfile") 
+	if os.path.exists(self.vagrant_file) and os.path.getsize(self.vagrant_file) > 0:
+	    self.v = vagrant.Vagrant(self.vagrant_VAGRANTFILE_DIR)
+	    os.chdir(self.vagrant_VAGRANTFILE_DIR)
+	    fd = open(self.vagrant_file, 'r')
+	    data = fd.read()
+	    if re.search(r'REQUIRED_PLUGINS = .* (vagrant-registration) .*',data):
+	        self.registration_required = True
+	    if os.getenv('SUB_USERNAME') and os.getenv('SUB_PASSWORD'):
+		self.credentials_exported = True	
+	else:
+	    print "Please check the vagrant file and re-run the test"
+	    sys.exit(1)
 
     def vagrant_status(self):
     	''' checks status of vagrant box and returns the state '''
@@ -27,23 +42,37 @@ class VagrantSanity(Test):
     	''' destroy old box and bring up a the vagrant box and finally destroy '''
 	self.log.info("Destroying old vagrant box, if any...")
 	self.test_vagrant_destroy()
-	#cmd = "vagrant up --provider %s" %(self.vagrant_PROVIDER)
+	cmd = "vagrant up --provider %s" %(self.vagrant_PROVIDER)
 	self.log.info("Brining up the vagrant box...")
-	child = pexpect.spawn ('vagrant up')
-	child.expect('.*Would you like to register the system now.*', timeout=300)
-	child.sendline ('n')
-	time.sleep(60)
+	child = pexpect.spawn (cmd)
+	if self.registration_required and not self.credentials_exported:
+	    child.expect('.*Would you like to register the system now.*', timeout=300)
+	    child.sendline ('n')
+	    self.log.info(child.after)
+	index = child.expect (['.*assword.*', pexpect.EOF, pexpect.TIMEOUT], timeout=300)
+	if index == 0:
+	    child.sendline (self.sudo_PASSWORD)
+	rc = child.expect(pexpect.EOF, timeout=None)
+	self.assertEqual(0, rc, "Vagrant up returned non-zero exit code")
 	out = self.vagrant_status()
-	self.assertEqual("running", out)
+	self.assertEqual("running", out, "The vagrant box is not running...")
 	self.test_vagrant_destroy()
 
     def test_vagrant_up_register(self):
     	''' vagrant up with registration to RHN '''
-        #cmd = "vagrant up --provider %s" %(self.vagrant_PROVIDER)
+	self.test_vagrant_destroy()
         self.log.info("Brining up the vagrant box and registering to RHN...")
         os.environ["SUB_USERNAME"] = self.vagrant_RHN_USERNAME
 	os.environ["SUB_PASSWORD"] = self.vagrant_RHN_PASSWORD
-	self.v.up()
+	cmd = "vagrant up --provider %s" %(self.vagrant_PROVIDER)
+	#child = popen_spawn.PopenSpawn (cmd)
+	child = pexpect.spawn (cmd)
+	index = child.expect (['.*assword.*', pexpect.EOF, pexpect.TIMEOUT], timeout=300)
+	if index == 0:
+            child.sendline (self.sudo_PASSWORD)
+            self.log.info(child.after)
+	rc = child.expect(pexpect.EOF, timeout=None)
+	self.assertEqual(0, rc)
 	out = self.vagrant_status()
 	self.assertEqual("running", out)
 
@@ -61,8 +90,7 @@ class VagrantSanity(Test):
         self.log.info("Suspending the vagrant box...")
         self.v.suspend()
         out = self.vagrant_status()
-	self.assertTrue(out is "paused" or "saved")
-
+	self.assertTrue(out in self.suspended_state)
 
     def test_vagrant_resume(self):
     	''' resume the suspended box '''
@@ -77,13 +105,19 @@ class VagrantSanity(Test):
         self.log.info("Halting the vagrant box...")
         self.v.halt()
         out = self.vagrant_status()
-        self.assertTrue(out is "shutoff" or "poweroff")
-
+	self.assertTrue(out in self.halt_state)
 
     def test_vagrant_reload(self):
     	''' reload the halted box '''
         self.log.info("Reloading the vagrant box...")
-        self.v.reload()
+        #cmd = "self.v.reload()"
+	cmd = "vagrant reload"
+	child = pexpect.spawn (cmd)
+        index = child.expect (['.*assword.*', pexpect.EOF, pexpect.TIMEOUT], timeout=300)
+	if index == 0:
+            child.sendline (self.sudo_PASSWORD)
+        rc = child.expect(pexpect.EOF, timeout=None)
+        self.assertEqual(0, rc)
 	out = self.vagrant_status()
         self.assertEqual("running", out)
 

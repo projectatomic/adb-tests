10b2908360bd7925cac37e4f9891b60da6152de5
amitkrout <amit.silicon2008@hotmail.com>
2016-10-01 14:10:03 +0530

    Default time out 60 sec set in utils
    Default time out 60 sec set in utils

--- cdk-testsuite/utils/openshiftUtils.py
+++ cdk-testsuite/utils/openshiftUtils.py
@@ -4,6 +4,9 @@ Created on Jun 29, 2016
 @author: amit
 '''
 
+from avocado.utils import process
+import time
+
 version = '1.0'
 release = ''
 
@@ -12,3 +15,26 @@ def get_version():
     Return openshift library version
     '''
     return version +" " + release
+
+def wait_for_output(cmd, timeout = 60):
+    output = "FAIL"
+    for i in range(timeout):
+        time.sleep(1)
+        try:
+            output = process.system_output(cmd)
+            return output
+        except:
+            pass
+    return output
+
+def wait_for_text_in_output(cmd, text = '', timeout = 60):
+    output = "FAIL"
+    for i in range(timeout):
+        time.sleep(1)
+        try:
+            output = process.system_output(cmd)
+            if text in output:
+                return output
+        except:
+            pass
+    return output
